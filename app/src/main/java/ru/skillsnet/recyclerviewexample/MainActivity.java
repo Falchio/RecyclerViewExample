6b83606a4c796f82baedace1f89e0f822f109175
Falchio <53504233+Falchio@users.noreply.github.com>
2020-02-27 15:39:48 +0300

    RecyclerView Example

---
+++ app/src/main/java/ru/skillsnet/recyclerviewexample/MainActivity.java
@@ -0,0 +1,40 @@
+package ru.skillsnet.recyclerviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] data = getResources().getStringArray(R.array.items);
+        initRecyclerView(data);
+    }
+
+    private void initRecyclerView(String[] data){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        // Эта установка служит для повышения производительности системы
+//        recyclerView.setHasFixedSize(true);
+
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(this,  LinearLayoutManager.VERTICAL);
+        itemDecoration.setDrawable(getDrawable(R.drawable.separator));
+        recyclerView.addItemDecoration(itemDecoration);
+
+
+        // Будем работать со встроенным менеджером
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Установим адаптер
+        SocnetAdapter adapter = new SocnetAdapter(data);
+        recyclerView.setAdapter(adapter);
+    }
+}
